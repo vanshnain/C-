53d2a75d66d24f3ecec073e0b67fb90242aefb48
Vansh Nain <vanshhnain@gmail.com>
2025-03-17 10:37:29 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -2,7 +2,6 @@ Program-1 With Output
 
 #include <cmath>
 using namespace std;
-
 double seriesSum(int n) {
     double sum = 0;
     for (int i = 1; i <= n; i++) {
@@ -15,14 +14,11 @@ double seriesSum(int n) {
     }
     return sum;
 }
-
 int main() {
     int n;
     cout << "Enter the number of terms: ";
     cin >> n;
-
     cout << "Sum of series: " << seriesSum(n);
     cout << endl;
 }
-
 <img width="826" alt="image" src="https://github.com/user-attachments/assets/e493e7f9-9bad-4ad6-8afb-490100d32cee" />
