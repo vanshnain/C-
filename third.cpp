47031a178724ded493d9ed1ab99334d1230ba41a
Vansh Nain <vanshhnain@gmail.com>
2025-03-17 10:41:01 +0530

    Update and rename third.md to third.cpp

---
+++ third.cpp
@@ -0,0 +1,27 @@
+Program-3 With Output
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+void countOccurrences(string str) {
+    map<char, int> freq;
+    for (char c : str) {
+        if (isalpha(c)) {
+            freq[tolower(c)]++;
+        }
+    }
+    cout << "Character Frequency Table:\n";
+    for (auto pair : freq) {
+        cout << pair.first << " -> " << pair.second << endl;
+    }
+}
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    countOccurrences(input);
+    return 0;
+}
+
+
+<img width="839" alt="image" src="https://github.com/user-attachments/assets/a6803e01-8d20-40de-91d8-a753fbd670f4" />
