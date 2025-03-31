a494a22bc3a6c77e83d0ac2d36d31faa18b98e78
Vansh Nain <vanshhnain@gmail.com>
2025-03-31 12:49:35 +0530

    Update 11th.cpp

--- 11th.cpp
+++ 11th.cpp
@@ -1,19 +1,19 @@
 Program-11 with output
-#include <iostream>
-#include <stdexcept>
-using namespace std;
-class MatrixException : public exception {
+#include <iostream> // Includes the iostream library for input and output operations
+#include <stdexcept> // Includes the stdexcept library for handling standard exceptions
+using namespace std; // allow functions
+class MatrixException : public exception { // Define a custom exception class that inherits from the standard exception class
 public:
-    const char* what() const noexcept { return "Matrix dimensions are incompatible!"; }
+    const char* what() const noexcept { return "Matrix dimensions are incompatible!"; } // Return the custom error message
 }
-void checkCompatibility(int c1, int r2) {
-    if (c1 != r2) throw MatrixException();
+void checkCompatibility(int c1, int r2) { // If the number of columns of matrix 1 is not equal to the number of rows of matrix 2
+    if (c1 != r2) throw MatrixException();  //  Throw the custom MatrixException
 }
 int main() {
-    try {
-        checkCompatibility(6, 4);
+    try { 
+        checkCompatibility(6, 4);  // Catch the MatrixException
     } catch (MatrixException& e) {
-        cout << "Error: " << e what() 
+        cout << "Error: " << e what() // print error
           cout<<endl;
     }
 }
