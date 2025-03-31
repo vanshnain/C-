09edd3dadb59689208f9bd68f9c15ec4ba22f432
Vansh Nain <vanshhnain@gmail.com>
2025-03-31 12:50:10 +0530

    Update 12th.cpp

--- 12th.cpp
+++ 12th.cpp
@@ -1,14 +1,13 @@
-12th-program with output
-#include <iostream>
-#include <stdexcept>
-using namespace std;
-class PrimeException : public exception {
+#include <iostream> // Includes the iostream library for input and output operations
+#include <stdexcept> // Includes the stdexcept library for handling standard exceptions
+using namespace std; // allow functions
+class PrimeException : public exception { 
 public:
     const char* what() const noexcept { return "Number must be greater than 1!"; }
 }
 bool isPrime(int num){
     if (num < 2) throw PrimeException();
-    for (int i = 2; i * i <= num; i++) 
+    for (int i = 2; i * i <= num; i++)  // for loop
         if (num % i == 0) return false;
     return true;
 }
@@ -17,12 +16,9 @@ int main(){
         int num;
         cout << "Enter a number: ";
         cin >> num;
-        cout << (isPrime(num) ? "Prime" : "Not Prime") << endl;
+        cout << (isPrime(num) ? "Prime" : "Not Prime") cout<<endl;
     } catch (PrimeException& e) {
         cout << e.what()
 cout<<endl;
     }
 }
-
-
-
