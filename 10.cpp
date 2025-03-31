dc9daf744b340ca3e6b240a1a8e859b3d8ddd7d1
Vansh Nain <vanshhnain@gmail.com>
2025-03-31 12:49:10 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -1,18 +1,16 @@
-program-10 with output
-#include <iostream>
-#include <cmath>
-using namespace std;
-class Triangle {
+#include <iostream> // (this is for input and output like cout and cin)
+#include <cmath> // (cmath library for maths)
+using namespace std; // allow functions
+class Triangle { // // Define a class named Triangle, A class is a user-defined data type
 public:
-    double area(double b, double h) { return 0.5 * b * h; }
-    double area(double a, double b, double c){
-        double s = (a + b + c)/2;
-        return sqrt(s*(s - a)*(s - b)*(s - c));
+    double area(double b, double h) { return 0.5 * b * h; } // formula 
+    double area(double a, double b, double c){  
+        double s = (a + b + c)/2; // formula
+        return sqrt(s*(s - a)*(s - b)*(s - c)); // formula
     }
 }
 int main() {
-    Triangle t;
+    Triangle t; // t is defined as triangle
     cout << "Area (Base, Height): " << t.area(7, 30) cout<<endl;
     cout << "Area (Three Sides): " << t.area(4, 7, 9) cout<<endl;
 }
-
